4fab16071acb48541ed86109fae75c5188a3bb90
Mikhail 'Bors1k' Novikov <8916misha@gmail.com>
2020-04-13 12:34:38 +0300

    Frontend login register verify added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/component/login/login.component'
+import { RegisterComponent } from './component/register/register.component';
+import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'verify-email', component: VerifyEmailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    VerifyEmailComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
